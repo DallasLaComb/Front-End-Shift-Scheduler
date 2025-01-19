745f90040715d7c1582bd6ac3e4bebb6871ac92a
Dallas LaComb <dallaslacombdrive@gmail.com>
2025-01-19 18:19:37 -0500

    Cloudfront added to CD pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>I Am Testing A CD Pipeline!</h1>
+      <h1>I Am Testing A CD Pipeline that clears CloudFront Cache!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
